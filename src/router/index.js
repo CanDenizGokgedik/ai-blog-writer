633c2d5839a7fc7fe153f28bef2d7a3c23e6ab06
Can Deniz Gökgedik <74368389+CanDenizGokgedik@users.noreply.github.com>
2025-05-19 01:06:58 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 const router = createRouter({ 
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    { 
       path: '/',
       name: 'home',
       component: HomeView
